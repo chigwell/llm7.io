53f7d7cb1047d395f3ec6a8e5d7679e6c90187cd
Eugene Evstafev <ee345@cam.ac.uk>
2025-12-14 09:32:13 +0000

    feat: comment out UsageSummaryChartCard component in page

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
       <FeaturedComponent />
       <ModelShowcase />
       <ActiveRequestsChart />
-      //<UsageSummaryChartCard />
       <Disclaimer/>
       <Footer />
     </>
